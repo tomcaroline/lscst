674d112e954d264b2471bed32bb9978335a3e9f7
tomcaroine <314725429@qq.com>
2015-11-08 21:32:13 +0800

    v0.0.1

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,86 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic'])
+    .config(function ($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+//设置android标签栏在底部
+        $ionicConfigProvider.platform.ios.tabs.style('standard');
+        $ionicConfigProvider.platform.ios.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('bottom');
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
+//设置android标签栏在底部
+//    路由设置
+        $stateProvider
+            .state('tabs', {
+                url: "/index",
+                templateUrl: "tpl/tabs.html"
+            })
+            .state('tabs.home', {
+                url: "/home",
+                views: {
+                    'home': {
+                        templateUrl: "tpl/home.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+            .state('tabs.O2O', {
+                url: "/O2O",
+                views: {
+                    'O2O': {
+                        templateUrl: "tpl/o2o.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+            .state('tabs.user', {
+                url: "/user",
+                views: {
+                    'user': {
+                        templateUrl: "tpl/user.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+            .state('tabs.toutiao', {
+                url: "/toutiao",
+                views: {
+                    'home': {
+                        templateUrl: "tpl/toutiao.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+            .state('tabs.paike', {
+                url: "/paike",
+                views: {
+                    'home': {
+                        templateUrl: "tpl/paike.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+            .state('tabs.fangcan', {
+                url: "/fangcan",
+                views: {
+                    'home': {
+                        templateUrl: "tpl/fangcan.html",
+                        //controller: "IndexController"
+                    }
+                }
+            })
+        $urlRouterProvider.otherwise("/index/home");//默认路由
+    })
+
+
